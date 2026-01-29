b28f879bcacf67741801f57f6ae8f7efa49259e3
yared-ayele-debela <yaredayele67@gmail.com>
2026-01-30 00:39:02 +0100

    feat(tournament): Update bootstrap configuration

--- distributed/tournament-service/bootstrap/app.php
+++ distributed/tournament-service/bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ValidatePassportToken;
-use Throwable;
+// use Throwable;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',

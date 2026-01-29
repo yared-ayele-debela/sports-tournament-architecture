dfff950bb89cf41bbcd3c895955042cf549b3eae
yared-ayele-debela <yaredayele67@gmail.com>
2026-01-30 00:35:17 +0100

    feat(team): Update bootstrap configuration
    - Enhanced service provider registration
    - Improved application bootstrap process
    - Better dependency injection setup

--- distributed/team-service/bootstrap/app.php
+++ distributed/team-service/bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Throwable;
+// use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(

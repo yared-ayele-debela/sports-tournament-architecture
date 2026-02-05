719a143aa9a9350f57777c59923b3037d27faf90
yared-ayele-debela <yaredayele67@gmail.com>
2026-02-05 01:45:14 +0100

    feat: add cached models and database migrations

---
+++ distributed/tournament-service/database/migrations/2026_02_04_203616_create_cached_tournament_teams_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cached_tournament_teams', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cached_tournament_teams');
+    }
+};

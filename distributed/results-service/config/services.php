deb7d78cc4a3218787494dd25e79690a686ac999
yared-ayele-debela <yaredayele67@gmail.com>
2026-02-09 23:25:35 +0100

    update results-service config

--- distributed/results-service/config/services.php
+++ distributed/results-service/config/services.php
@@ -36,20 +36,20 @@ return [
     ],
 
     'auth' => [
-        'url' => env('AUTH_SERVICE_URL', 'http://localhost:8001'),
+        'url' => env('AUTH_SERVICE_URL', 'http://auth-service:8001'),
         'token_cache_ttl' => env('AUTH_TOKEN_CACHE_TTL', 300), // 5 minutes default
     ],
 
     'team_service' => [
-        'url' => env('TEAM_SERVICE_URL', 'http://localhost:8003'),
+        'url' => env('TEAM_SERVICE_URL', 'http://team-service:8003'),
     ],
 
     'match_service' => [
-        'url' => env('MATCH_SERVICE_URL', 'http://localhost:8004'),
+        'url' => env('MATCH_SERVICE_URL', 'http://match-service:8004'),
     ],
 
     'tournament_service' => [
-        'url' => env('TOURNAMENT_SERVICE_URL', 'http://localhost:8002'),
+        'url' => env('TOURNAMENT_SERVICE_URL', 'http://tournament-service:8002'),
     ],
 
 ];
